f3b185b5114542e57d50721d545df58b004ae583
akashdktyagi <akashdktyagi@gmail.com>
2020-12-19 19:12:17 +0800

    first verson

---
+++ src/test/java/com/visionit/TestClass2.java
@@ -0,0 +1,22 @@
+package com.visionit;
+
+public class TestClass2 {
+
+    public void test_01(){
+        System.out.println("I am test method 1");
+    }
+
+    public void test_02(){
+        System.out.println("I am test method 1");
+    }
+
+    public void test_03(){
+        System.out.println("I am test method 1");
+    }
+    
+    //Branch:case-2-changing-test-file-2-by-sarang
+    public void test_04(){
+        System.out.println("I am test method 4");
+    }
+
+}
